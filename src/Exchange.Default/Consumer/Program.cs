c80d78d16f59b40634eade26e5822ef69816d95e
indianbleach <84047808+IndianBleach@users.noreply.github.com>
2022-08-25 09:32:17 +0300

    add default exchange, pub and consumer

---
+++ src/Exchange.Default/Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+Thread.Sleep(2000);
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("dev-queue",
+            false, false, false, null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, e) =>
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body.ToArray());
+
+            Console.WriteLine("Message recieved:" + message);
+        };
+
+        channel.BasicConsume("dev-queue", true, consumer);
+    }
+}
---
+++ src/Exchange.Default/Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("dev-queue",
+            false, false, false, null);
+
+        string message = "Message from Publisher";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("",
+            "dev-queue",
+            null,
+            body);
+
+        Console.WriteLine("Message sent with default exchange");
+    }
+}
+
