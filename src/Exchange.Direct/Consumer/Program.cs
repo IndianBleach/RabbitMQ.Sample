13872baeeddecf9e5a89f2bf7a72938a5067918d
indianbleach <84047808+IndianBleach@users.noreply.github.com>
2022-08-26 14:09:50 +0300

    add direct exchange

---
+++ src/Exchange.Direct/Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var fact = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    Password = "guest",
+    UserName = "guest"
+};
+
+using (IConnection connection = fact.CreateConnection())
+{
+    using (IModel channel = connection.CreateModel())
+    {
+        channel.ExchangeDeclare("direct_logs", ExchangeType.Direct);
+
+        string queueName = channel.QueueDeclare().QueueName;
+
+        channel.QueueBind(queue: queueName,
+            exchange: "direct_logs",
+            routingKey: "info");
+
+        EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, e) =>
+        {
+            string message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            Console.WriteLine($"Message Recieved [RouteKey]: info, [Message]: " + message);
+        };
+
+        channel.BasicConsume(queueName, true, consumer);
+
+        Console.WriteLine("Info consume");
+
+        Console.ReadLine();
+    }
+}
---
+++ src/Exchange.Direct/Publisher/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Task.Run(StartPublisher(2000, "error"));
+
+Task.Run(StartPublisher(2000, "warning"));
+
+Task.Run(StartPublisher(2000, "info"));
+
+
+static Func<Task> StartPublisher(int sleepTime, string routingKey)
+{
+    return () =>
+    {
+        int counter = 0;
+
+        do
+        {
+            Thread.Sleep(sleepTime);
+
+            var fact = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                Password = "guest",
+                UserName = "guest"
+            };
+
+            using (IConnection connection = fact.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("direct_logs", ExchangeType.Direct);
+
+                    string message = string.Format("[{1}] New message with key: {0}", routingKey, counter);
+
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "direct_logs",
+                        routingKey: routingKey,
+                        body: body,
+                        basicProperties: null);
+
+                    Console.WriteLine(message + " - sent");
+                }
+            }
+        }
+        while (true);
+    };
+}
+
+Console.ReadLine();
